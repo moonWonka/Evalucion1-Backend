1b7e4a99914563daa0e0c4c41639b41d9ed97123
willy <w.lunah@protonmail.com>
2023-05-07 20:03:05 -0400

    casi terminado

--- Django_Evaluacion_1/settings.py
+++ Django_Evaluacion_1/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'primeraEvaluacion.apps.PrimeraevaluacionConfig'
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'Django_Evaluacion_1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join( BASE_DIR, 'plantillas')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- primeraEvaluacion/views.py
+++ primeraEvaluacion/views.py
@@ -1,3 +1,118 @@
+import datetime
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template, Context
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'template/index.html')
+
+def usuario(request):
+    documento = open("plantilla/template/usuario.html")
+    plt = Template(documento.read())
+
+    documento.close()
+
+    ctx = Context({
+        "nombre": "Juan",
+        "apellido": "Perez", 
+        "edad": 25, 
+        "fecha": datetime.datetime.now()
+        })
+        
+    pagina = plt.render(ctx)
+
+    return HttpResponse(pagina)
+
+def productos(request):
+
+    categorias = [
+        {
+            "nombre": "videojuegos",
+            "items": [
+                {
+                    "producto": "FIFA 22",
+                    "descripcion": "Simulador de fútbol",
+                    "imagen" : "videojuegos/fifa2022.webp",
+                    "precio": 49990
+                },
+                {
+                    "producto": "PES 22",
+                    "descripcion": "Simulador de fútbol",
+                    "imagen" : "videojuegos/pes2022.webp",
+                    "precio": 39990
+                },
+                {
+                    "producto": "GTA V",
+                    "descripcion": "Videojuego de acción y aventuras",
+                    "imagen" : "videojuegos/gtav.webp",
+                    "precio": 29990
+                }
+            ]
+        },
+        {
+            "nombre": "ropa",
+            "items": [
+                {
+                    "producto": "Camiseta",
+                    "descripcion": "Camiseta de algodón para hombres",
+                    "precio": 14990
+                },
+                {
+                    "producto": "Pantalon",
+                    "descripcion": "Pantalón de mezclilla para mujeres",
+                    "precio": 29990
+                },
+                {
+                    "producto": "Zapatos",
+                    "descripcion": "Zapatos de vestir para hombres",
+                    "precio": 59990
+                }
+            ]
+        },
+        {
+            "nombre": "juguetes",
+            "items": [
+                {
+                    "producto": "Carro",
+                    "descripcion": "Carro de juguete para niños",
+                    "precio": 4990
+                },
+                {
+                    "producto": "Muñeca",
+                    "descripcion": "Muñeca de trapo para niñas",
+                    "precio": 9990
+                },
+                {
+                    "producto": "Pelota",
+                    "descripcion": "Pelota de fútbol para niños",
+                    "precio": 2990
+                }
+            ]
+        },
+    ]
+    
+    nombreCategoria = request.GET.get('categoria')
+
+    data = {}
+
+    if nombreCategoria:
+        
+        for categoria in categorias:
+
+            if categoria["nombre"] == nombreCategoria:
+
+                data = {"categoria": categoria["nombre"],
+                        "items": categoria["items"],
+                        "fecha": datetime.datetime.now()
+                        }
+                break
+                
+    return render(request, 'template/productos.html', data)
+
+# def juguetes(request):
+#     return render(request, 'template/juguetes.html')
+
+# def ropa(request):
+#     return render(request, 'template/ropa.html')
